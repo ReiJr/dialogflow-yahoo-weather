5f436e1a3ec584c70a39eb20bdb7154effd2bf99
ReiJr <reinaldo.isj@hotmail.com>
2018-09-12 13:51:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@
 # Author - Naresh Ganatra
 # http://youtube.com/c/NareshGanatra
 
-
+'''
 from __future__ import print_function
 #from future.standard_library import install_aliases
 #install_aliases()
@@ -30,10 +30,11 @@ from urllib.error import HTTPError
 
 import json
 import os
-
+'''
+import os
 from flask import Flask
-from flask import request
-from flask import make_response
+#from flask import request
+#from flask import make_response
 
 # Flask app should start in global layout
 app = Flask(__name__)
